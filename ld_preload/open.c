7cda1d7bf99f85e96fb5a32cd0b1ea7374bdd72e
Hsin2Code <hsin2code@gmail.com>
2016-07-06 17:16:19 +0800

    带有构造函数属性的函数

--- ld_preload/open.c
+++ ld_preload/open.c
@@ -7,6 +7,26 @@
 
 typedef int (*orig_open_f_type)(const char *__file, int __oflag, ...); 
 
+/* 带有"构造函数"属性的函数将在main()函数之前被执行，
+ * 而声明为"析构函数"属性的函数则将在main()退出时执行。
+ * 全局类的构造过程发生在"__attach"函数之前,而析构也发生在__detach()前面。
+ */
+/* 修改 /etc/ld.so.preload */
+void __attribute__ ((constructor)) __attach(void);
+void __attribute__ ((destructor)) __detach(void);
+
+void __attach(void)
+{
+    return;
+    
+}
+
+void __detach(void)
+{
+    return;
+    
+}
+
 int open(const char *__file, int __oflag, ...)
 {
     orig_open_f_type orig_open = NULL;
